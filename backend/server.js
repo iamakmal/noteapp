7750ef913f05c127daa3ed079d2a6b96a70c431a
iamakmal <m.akmalalavi@gmail.com>
2024-03-18 15:47:05 +0530

    fixed bugs in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ const app = express();
 require("dotenv").config({path:"./.env"});
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 const noteRoutes = require('./routes/notesRoutes')
 
 const db = process.env.MONGO_URI;
 mongoose.connect(db, {
     useNewUrlParser: true,
-    useUnifiesTopology: true
+    useUnifiedTopology: true
 });
 
 const connection = mongoose.connection;
@@ -21,11 +24,5 @@ app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}))
 app.use("/api/notes", noteRoutes);

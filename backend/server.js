5b33148ecf1b26265d5f7891cf9bfe487acc3373
iamakmal <m.akmalalavi@gmail.com>
2024-03-18 17:23:15 +0530

    Completed Backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-require("dotenv").config({path:"./.env"});
+require("dotenv").config({path:"../.env"});
 const port = process.env.PORT || 5000;
 
 app.use(express.json());

e855934e6c4a67bb1ca5a3a6a4d2504fec318f1d
iamakmal <m.akmalalavi@gmail.com>
2024-03-18 15:24:38 +0530

    created notesRoutes

---
+++ backend/routes/notesRoutes.js
@@ -0,0 +1,10 @@
+const route = require('express').Router()
+const {createNote,getAllNotes,getNoteById,updateNote,deleteNote} = require('../controller/notesController')
+
+route.post("/createNote", createNote)
+route.get("/getNotes", getAllNotes)
+route.get("/getNote/:id",getNoteById)
+route.put("/updateNote:id",updateNote)
+route.delete("/deleteNote:id",deleteNote)
+
+module.exports = route

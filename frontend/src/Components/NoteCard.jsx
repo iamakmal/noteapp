0f6c89e6fad76203db9172ea76afb2110a577094
iamakmal <m.akmalalavi@gmail.com>
2024-03-19 15:30:35 +0530

    Made Changes to NoteCard

--- frontend/src/Components/NoteCard.jsx
+++ frontend/src/Components/NoteCard.jsx
@@ -4,6 +4,8 @@ import axios from "axios"
 
 export default function NoteCard(props) {
     const { _id, title, body, updatedAt } = props.notes
+    const newDate = new Date(updatedAt)
+    const date = newDate.toLocaleDateString() + " at " + newDate.toLocaleTimeString()
     const id = _id.toString()
     let navigate = useNavigate();
 
@@ -22,8 +24,8 @@ export default function NoteCard(props) {
                 <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white text-left">{title}</h5>
                 <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400 text-left overflow-ellipsis">{body}</p>
                 <div class="flex justify-between items-center">
-                <p class="text-base text-gray-600 sm:text-lg dark:text-gray-400 text-left">{updatedAt}</p>
-                    <div class="mt-10 flex items-center gap-x-6 mr-8">
+                <p class="mt-10 text-base text-gray-500 sm:text-lg dark:text-gray-400 text-left">{date.toString()}</p>
+                    <div class="mt-10 flex items-center gap-x-6 mr-5">
                         <a
                             class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-right text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         ><Link to={{ pathname: `/updateNote/${id}`}} >
